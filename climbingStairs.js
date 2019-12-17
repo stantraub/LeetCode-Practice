6404dbcb5ad2da08b1023249471130db7b468295
Stanley <stanley.traub@gmail.com>
2019-12-16 19:50:06 -0800

    Climbing stairs

---
+++ climbingStairs.js
@@ -0,0 +1,11 @@
+function climbStairs(n) {
+    let table = new Array(n + 1);
+    table[0] = 1;
+    table[1] = 1;
+
+    for (let i = 2; i < table.length; i++) {
+        table[i] = table[i - 1] + table[i - 2];
+    }
+
+    return table[table.length - 1];
+}
