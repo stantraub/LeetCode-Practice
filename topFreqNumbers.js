4ccd04fc6d8af7dcb138e147005389b4070ff386
Stanley <stanley.traub@gmail.com>
2019-12-19 14:35:12 -0800

    Finished topKFrequent

---
+++ topFreqNumbers.js
@@ -0,0 +1,21 @@
+var topKFrequent = function (nums, k) {
+    let freq = {};
+
+    nums.forEach(num => {
+        freq[num] ? freq[num] += 1 : freq[num] = 1;
+    })
+
+    let res = [];
+
+    let entries = Object.entries(freq).sort((a, b) => b[1] - a[1])
+
+    for (let i = 0; i < k; i++) {
+        res.push(parseInt(entries[i], 0));
+    }
+
+    return res;
+
+
+};
+
+console.log(topKFrequent([3,0,1,0], 1))
