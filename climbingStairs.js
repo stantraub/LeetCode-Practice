d2f5a84b3afc77a8fb73b6fc835c8e7dae257387
Stanley <stanley.traub@gmail.com>
2019-12-31 21:19:21 -0800

    Refactored climbing stairs

--- climbingStairs.js
+++ climbingStairs.js
@@ -3,7 +3,7 @@ function climbStairs(n) {
     table[0] = 1;
     table[1] = 1;
 
-    for (let i = 2; i < table.length; i++) {
+    for (let i = 2; i <= n; i++) {
         table[i] = table[i - 1] + table[i - 2];
     }
 
