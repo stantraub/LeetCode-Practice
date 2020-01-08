99bba4090adbb234318ab1f0b23f421a34c7cde9
Stanley <stanley.traub@gmail.com>
2020-01-08 09:24:32 -0800

    Finished minPathSum

---
+++ minPathSum.js
@@ -0,0 +1,24 @@
+// O(m * n) time 
+function minPathSum(grid) {
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[0].length; j++) {
+            if (i === 0 && j === 0) {
+                continue
+            } else if (i === 0) {
+                grid[i][j] = grid[i][j] + grid[i][j - 1]
+            } else if (j === 0) {
+                grid[i][j] = grid[i][j] + grid[i - 1][j]
+            } else {
+                grid[i][j] = grid[i][j] + Math.min(grid[i - 1][j], grid[i][j - 1])
+            }
+        }
+    }
+
+    return grid[grid.length - 1][grid[0].length - 1]
+}
+
+console.log(minPathSum([
+    [1, 3, 1],
+    [1, 5, 1],
+    [4, 2, 1]
+])) // => 7
