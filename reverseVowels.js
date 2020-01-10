63bc9625b342e44d6ea40137c74b0d608cc34f23
Stanley <stanley.traub@gmail.com>
2020-01-10 10:35:59 -0800

    Finished reverse vowels of string

---
+++ reverseVowels.js
@@ -0,0 +1,34 @@
+function reverseVowels(s) {
+    let i = 0;
+    let j = s.length - 1;
+
+    let arr = s.split("")
+    let vowels = {
+        a : true,
+        e : true,
+        i : true,
+        o : true,
+        u : true
+    }
+
+    while (i < j) {
+        while (!vowels[arr[i].toLowerCase()] && i < j) {
+            i++
+        }
+
+        while (!vowels[arr[j].toLowerCase()] && i < j) {
+            j--
+        }
+
+        let swap = arr[i];
+        arr[i] = arr[j];
+        arr[j] = swap;
+
+        i++;
+        j--;
+    }
+
+    return arr.join("");
+}
+
+console.log(reverseVowels('hello')) //=> 'holle'
