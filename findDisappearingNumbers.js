1ac3810677ae146a5a7b9b530b7710266cdf5054
Stanley <stanley.traub@gmail.com>
2020-01-15 11:08:00 -0800

    Finished disappearing numbers

---
+++ findDisappearingNumbers.js
@@ -0,0 +1,15 @@
+var findDisappearedNumbers = function (nums) {
+    let res = [];
+    let numbers = new Set();
+    for (const num of nums) {
+        numbers.add(num)
+    }
+
+    for (let i = 1; i <= nums.length; i++) {
+        if (!numbers.has(i)) {
+            res.push(i)
+        }
+    }
+
+    return res
+};
