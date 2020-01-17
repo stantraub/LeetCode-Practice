9898f7f0a91327491aebc5f0115d8483bfae1f7b
Stanley <stanley.traub@gmail.com>
2020-01-17 13:58:56 -0800

    Finished move zeroes

---
+++ moveZeros.js
@@ -0,0 +1,17 @@
+var moveZeroes = function (nums) {
+    let pos = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== 0) {
+            nums[pos] = nums[i];
+            pos++
+
+        }
+    }
+
+    for (let i = pos; i < nums.length; i++) {
+        nums[i] = 0;
+    }
+
+
+};
