efc076cc1210c8c0b217012c5529433bb62c4a69
Stanley <stanley.traub@gmail.com>
2020-02-21 17:50:45 -0800

    Finished remove element

---
+++ removeElement.js
@@ -0,0 +1,12 @@
+var removeElement = function (nums, val) {
+    let i = 0
+
+    for (const num of nums) {
+        if (num !== val) {
+            nums[i] = num
+            i++
+        }
+    }
+    console.log(nums)
+    return i
+};
