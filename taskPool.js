bba8367d3eaaaf9612c7cb550ff56df79d59cf22
Stanley <stanley.traub@gmail.com>
2020-05-29 13:26:59 -0700

    Async/await refactor of task pool

--- taskPool.js
+++ taskPool.js
@@ -3,13 +3,11 @@ class TaskPool {
         this.queue = []
     }
 
-    run() {
-        return Promise.all(this.queue)
-        .then((res) => {
-            this.queue = []
-            console.log(res)
-        })
-        // .then(() => console.log('Ready to accept new tasks'))
+    async run() {
+        const res = await Promise.all(this.queue)
+        this.queue = []
+        console.log(res)
+        console.log('Ready to accept new tasks')
     }
 }
 
@@ -25,19 +23,18 @@ const maxConcurrency = async (max) => {
 
     let pool = new TaskPool(max)
 
-    for  (const url of urls) {
-        if (pool.queue.length < max) {
-            pool.queue.push(url)
-            if (pool.queue.length === max) {
-                await pool.run()
-            }
-        } else {
+    for (const url of urls) {
+        if (pool.queue.length === max) {
             await pool.run()
+            pool.queue.push(url)
+        } else {
             pool.queue.push(url)
         }
     }
 
+    if (pool.queue.length > 0) await pool.run()
+
 
 }
 
-maxConcurrency(3)
+maxConcurrency(2)
